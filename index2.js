2ead096bc08a111a79eb8242c38f7488fcdfa319
juanb <juan052115@gmail.com>
2024-08-13 22:17:48 -0600

    cambio para --rebase

--- index2.js
+++ index2.js
@@ -3,3 +3,4 @@ console.log("este es un cambio desde la rama y sera mergeado por un cambio de ma
 console.log("este commit si se debe subir")
 console.log("subire este cambio mientras tengo la rama main en stash")
 console.log("subeme con stash")
+//aca se supone que trabaje el index2 y luego voy a subir estos cambios pero antes del push hago un pull con --rebase
