dde17ee0c00947d057b3c4547e7447f7225b5c4b
juanb <juan052115@gmail.com>
2024-08-13 22:07:28 -0600

    subiendo el index2

--- index2.js
+++ index2.js
@@ -1,3 +1,4 @@
 console.log("este es un archivo creado por la segunda rama")
 console.log("este es un cambio desde la rama y sera mergeado por un cambio de main generado por otra persona")
 console.log("este commit si se debe subir")
+console.log("subeme con stash")
